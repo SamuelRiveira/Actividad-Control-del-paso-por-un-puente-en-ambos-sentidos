2f6315a555426515f85e4359274f2f39ab9024c3
SamuelRiveira <160497907+SamuelRiveira@users.noreply.github.com>
2024-12-15 19:02:09 +0000

    Update Persona.java

--- puentethreadsafe/src/main/java/com/mycompany/puentethreadsafe/Persona.java
+++ puentethreadsafe/src/main/java/com/mycompany/puentethreadsafe/Persona.java
@@ -1,29 +1,33 @@
 package com.mycompany.puentethreadsafe;
 
 public class Persona implements Runnable {
-    // Atributos
     private final String idPersona;
     private final int tiempoPaso;
     private final int pesoPersona;
+    private final String sentido;
     private final Puente puente;
-    private final String sentido; // Dirección de la persona
 
-    // Constructor
     public Persona(String idPersona, int tiempoPaso, int pesoPersona, String sentido, Puente puente) {
         this.idPersona = idPersona;
         this.tiempoPaso = tiempoPaso;
         this.pesoPersona = pesoPersona;
-        this.puente = puente;
         this.sentido = sentido;
+        this.puente = puente;
     }
 
-    // Getters
+    @Override
+    public void run() {
+        try {
+            puente.entrar(this);  // Intentar entrar al puente
+            Thread.sleep(tiempoPaso * 100);  // Tiempo que tarda en cruzar
+            puente.salir(this);  // Salir del puente
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String getIdPersona() {
         return idPersona;
-    }
-
-    public int getTiempoPaso() {
-        return tiempoPaso;
     }
 
     public int getPesoPersona() {
@@ -32,29 +36,5 @@ public class Persona implements Runnable {
 
     public String getSentido() {
         return sentido;
-    }
-
-    // Método run()
-    @Override
-    public void run() {
-        System.out.printf(">>> La %s con %d kilos quiere cruzar hacia %s en %d segundos.\n",
-                idPersona, pesoPersona, sentido, tiempoPaso);
-
-        // Entrar
-        try {
-            puente.entrar(this);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Cruzar
-        try {
-            Thread.sleep(tiempoPaso + 100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Salir
-        puente.salir(this);
     }
 }
